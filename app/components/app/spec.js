409faca87baddcd346a5af3dd79526ab872e840c
olin <olin@xogrp.com>
2017-08-21 16:43:33 +0800

    setup unit testing

---
+++ app/components/app/spec.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { expect } from "chai"
+import { shallow } from "enzyme"
+
+import { App } from "./";
+
+describe("<App />", () => {
+  let wrapper, container
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App />
+    )
+  })
+
+  describe("render", () => {
+    it("content", () => {
+      container = wrapper.find(".react-content")
+
+      expect(container.text()).to.equal("react text")
+    })
+  })
+})
